ef2ad577a918d3765ff11a6498935a57113e27b4
ErellD <edelesvaux@gmail.com>
2022-12-13 15:59:23 +1100

    changed api key trying to fix bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -32,10 +32,10 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "46fac47dd8b8fa26d1b6852218ad3dfe";
+    let apiKey = "aa09763d916df0424c840d55bfc2d2c9";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric
 `;
 
     axios.get(apiUrl).then(handleResponse);
